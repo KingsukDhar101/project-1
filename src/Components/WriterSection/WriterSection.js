6fe94972402b83687fc7d7f829fd26eccf6b9717
kingsuk_1997 <kingsuk.1997dhar2@gmail.com>
2021-11-18 20:20:33 +0530

    modified json file - added fake api url

--- src/Components/WriterSection/WriterSection.js
+++ src/Components/WriterSection/WriterSection.js
@@ -1,48 +1,34 @@
-import React, {Component} from 'react'
-import './WriterSection.css'
-// import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+import "./WriterSection.css";
 
-class WriterSection extends Component{
-    constructor(props){
-        super(props)
-        this.state = {
-            user: [],
-        }
-    }
-    getUser(){
-        fetch('https://reqres.in/api/users?page=2')
-            .then(res => res.json())
-            .then(json => json.data) 
-            .then(data => {
-                this.setState({user: data});
-            })
-            .catch(err => console.log("Error --> " + err));
-    }
-    
-    componentDidMount(){
-        this.getUser();
-    }
-    
-    render(){
-        return(
-            <div className='writer-container'>
-                <h1 className='writer-header'>Featured Writers</h1>
-                <div className='writer-section'>
-                    {this.state.user && this.state.user.map((user)=>{
-                        return(
-                            <div className='user-card' key={user.id}>
-                                <img src={user.avatar} alt='image_'/>
-                                <h4>{user.first_name}</h4>
-                                <p>Email: {user.email}</p>
-                            </div>
-                        )
-                    })}
-                </div>
-            </div>
-        );
-    }
-    
-    
-}
+const WriterSection = () => {
+  const [users, setUsers] = useState([]);
 
-export default WriterSection
+  useEffect(() => {
+    const url = "https://reqres.in/api/users?page=2";
+    axios.get(url)
+      .then( res => setUsers(res.data.data))
+      // .then(data => console.log(data))
+      .catch(err => console.log(err))
+  }, []);
+
+  return (
+    <div className="writer-container">
+      <h1 className="writer-header">Featured Writers</h1>
+      <div className="writer-section">
+        {users && users.map((user) => {
+            return (
+              <div className="user-card" key={user.id}>
+                <img src={user.avatar} alt="image_" />
+                <h4>{user.first_name}</h4>
+                <p>Email: {user.email}</p>
+              </div>
+            );
+          })}
+      </div>
+    </div>
+  );
+};
+
+export default WriterSection;
